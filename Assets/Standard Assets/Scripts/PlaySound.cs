adfde1d24d2e6c60cab27b0fd5d51870c820fc68
kendobi <yokenny@gmail.com>
2014-10-28 20:37:50 -0700

    Entropy First Check-In

---
+++ Assets/Standard Assets/Scripts/PlaySound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudio : MonoBehaviour {
+	void Start() {
+		audio.Play();
+		//audio.Play(44100);
+	}
+}
